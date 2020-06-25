1abb1f457695a8b029c345bcf59bdba2d9a0fa33
mho9 <59281305+mho9@users.noreply.github.com>
2020-06-25 13:55:05 +0300

    Add files via upload

---
+++ sa.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+from random import choice
+
+print('''
+      ###########################
+
+      @ if you want random  movie just press Enter
+
+      #############################
+''')
+type_mo = input('Enter type : ')
+s = requests.Session()
+s.headers.update({
+  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+})
+if type_mo =='':
+    page = s.get('https://www.fushaar.com/')
+else:
+
+    page = s.get(f'https://www.fushaar.com/gerne/{type_mo}/')
+
+if page.status_code==200:
+  soup = BeautifulSoup(page.text, 'html.parser')
+  s = soup.select(".info h3")
+  movie_cho = choice(s)
+  print(movie_cho.get_text())
+
+elif page ==404:
+  print('[+] page not found')
+
